09311b258f2c19cbb2e2a2c1e2336f50a004c41f
shreeyantrapc20 <shreeyantrapc@gmail.com>
2025-01-21 07:24:28 +0545

    Ado.net Project

---
+++ ADONetProject1/DataAccessLayer/AssemblyReference.cs
@@ -0,0 +1,19 @@
+﻿//used for communicating with another project
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public static class AssemblyReference
+    {
+        public static IServiceCollection AddDAL(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
--- FirstProject/FirstProject/obj/Debug/net6.0/FirstProject.AssemblyInfo.cs
+++ FirstProject/FirstProject/obj/Debug/net6.0/FirstProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595546cba1ef3217f0c349ec3206250484061a48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d3ae950290130d93ed231a145fd009410a0ab15")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
